83266616347f2a92e3233bd30b94a2d61a80897a
mozes20 <htcmokka@gmail.com>
2021-02-23 19:54:50 +0100

    Base
    Base

---
+++ Assets/MScripts/Butteryscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Butteryscript : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetButtery(float buttery) {
+
+        slider.value = buttery;
+    }
+
+    public void SetMaxButtery(int buttery) {
+        slider.maxValue = buttery;
+        slider.value = buttery;
+    }
+
+
+}
---
+++ Assets/MScripts/DoorMechanik.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMechanik : MonoBehaviour
+{
+    [SerializeField] public bool closed = false;
+    [SerializeField] public int keyNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!closed)
+        {
+            JointLimits limits = this.GetComponent<HingeJoint>().limits;
+            limits.max = 90;
+            this.GetComponent<HingeJoint>().limits = limits;
+        }
+        else
+        {
+            JointLimits limits = this.GetComponent<HingeJoint>().limits;
+            limits.max = 1;
+            this.GetComponent<HingeJoint>().limits = limits;
+        }
+
+    }
+}
